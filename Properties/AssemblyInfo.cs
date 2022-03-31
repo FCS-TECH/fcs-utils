7e1897f8d5378f52fe5aab2bafe5fe9bcc246525
FH <fh@uex.dk>
2022-03-31 17:59:59 +0200

    added vat utils

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("aaf08873-14e5-411d-8ec8-629782ac8f03")]
 
-[assembly: AssemblyVersion("2.1.22074.1600")]
-[assembly: AssemblyFileVersion("2.1.22074.1600")]
+[assembly: AssemblyVersion("2.1.22090.1256")]
+[assembly: AssemblyFileVersion("2.1.22090.1256")]
